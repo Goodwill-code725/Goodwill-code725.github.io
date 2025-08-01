fbb501567ed628d6d25238f9649ca92e8a2b1ba8
Jeremy Amparo <108560831+Goodwill-code725@users.noreply.github.com>
2025-08-01 14:25:57 +0800

    updated 8/1/25

---
+++ script.js
@@ -0,0 +1,6 @@
+function openModal(id) {
+  document.getElementById(id + "-modal").style.display = "block";
+}
+function closeModal(id) {
+  document.getElementById(id + "-modal").style.display = "none";
+}
